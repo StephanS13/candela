b090b3c340a312d6b9408a9144dea55f7f7c7e71
Stephane Simcic <simcic.stephane@gmail.com>
2024-07-23 17:00:36 +0900

    Adds films page

--- src/pages/films.jsx
+++ src/pages/films.jsx
@@ -1,26 +1,25 @@
 import React from 'react'
+import ReactPlayer from 'react-player/vimeo';
+import '../assets/films.css'
+
 
 
 function Films() {
   return (
-    <div style={{marginTop: '0px'}}>
-      <div className="hero-1">
-        <div className="hero-1-title">
-          <h3>EVENEMENTIEL</h3>
-          <br />
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus doloribus quas esse! At atque minus sunt, quibusdam commodi porro, iure debitis, fugit nisi alias quidem reprehenderit nam nihil beatae sed.</p>
-        </div>
-        <div className="hero-1-pic"></div>
+    <div>
+      <div className="films-hero">
+       <h1>FILMS</h1>
       </div>
-      <div className="hero-2">
-        <div className="hero-2-pic"></div>
-        <div className="hero-2-title">
-          <h3>INSTITUTIONEL</h3>
-          <br />
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus odio suscipit maiores voluptatibus voluptates provident a, cumque nemo dolores rem animi recusandae dolor, vitae eligendi. Laborum molestias autem modi fugiat?</p>
-        </div>
+      <div className='films-container'>
+        <ReactPlayer 
+          url='https://vimeo.com/988278918' 
+          width='80%'
+          height='80%'
+          controls={true} 
+        />
       </div>
     </div>
+    
   )
 }
 
--- src/pages/photos.jsx
+++ src/pages/photos.jsx
@@ -10,7 +10,6 @@ function Photos() {
       </div>
       <Grid />
     </div>
-    
   )
 }
 
